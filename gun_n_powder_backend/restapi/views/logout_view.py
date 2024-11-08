f82c77bf594c5182c2d855c36d4cc798308b5788
Ryan Tan <ryantan.mail@gmail.com>
2024-11-08 07:44:34 -0800

    added csrf exempt

--- gun_n_powder_backend/restapi/views/logout_view.py
+++ gun_n_powder_backend/restapi/views/logout_view.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import logout
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt  # For testing purposes; handle CSRF more securely in production
 def api_logout(request):
     if request.method == "POST":
         logout(request)
