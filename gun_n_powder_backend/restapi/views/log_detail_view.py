bd991a6c0b58862e4794ee9f992c4e8bec008509
Ryan Tan <ryantan.mail@gmail.com>
2024-11-12 21:10:10 -0800

    change to body input

--- gun_n_powder_backend/restapi/views/log_detail_view.py
+++ gun_n_powder_backend/restapi/views/log_detail_view.py
@@ -1,5 +1,6 @@
 
 import os
+import json
 from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse, HttpResponse
 from rest_framework.views import csrf_exempt
@@ -7,10 +8,11 @@ from rest_framework.views import csrf_exempt
 
 @csrf_exempt
 @login_required
-def log_detail(request, filename):
+def log_detail(request):
     if request.method == "GET":
+        data = json.loads(request.body)
+        filename = data.get("filename")
         file_path = os.path.join("/container_zap", filename, ".txt")
-
         try:
             with open(file_path, "r") as file:
                 content = file.read()
