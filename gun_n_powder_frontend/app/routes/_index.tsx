55769def439238e5d95101aca87a5cc6dbb4e824
Harneet Dhillon <harneet.dhillon2020@gmail.com>
2024-11-13 22:47:56 -0800

    index, login, signup css added

--- gun_n_powder_frontend/app/routes/_index.tsx
+++ gun_n_powder_frontend/app/routes/_index.tsx
@@ -3,17 +3,20 @@ import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Gun n Powder" },
+    { name: "description", content: "AI Enhanced Penetration Testing" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center flex-col justify-center">
-      <h1 className="p-5"> Gun n Powder </h1>
-      <h1 className="p-5"> AI Enhanced Penetration Testing </h1>
-      <Link className="p-10" to="/login">
+    <div className="flex h-screen items-center flex-col justify-center bg-gray-300">
+      <h1 className="text-6xl font-light mb-10 text-black">Gun n Powder</h1>
+      <h2 className="text-2xl font-light mb-20 text-black">AI Enhanced Penetration Testing</h2>
+      <Link
+         className="px-6 py-2 text-lg font-light text-gray-700 border border-gray-700 rounded-md hover:bg-gray-200 transition duration-300"
+         to="/login"
+       >
         Get Started
       </Link>
     </div>
