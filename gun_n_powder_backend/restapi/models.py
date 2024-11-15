06ef337827224520a437e65d632b266d681a8fb6
Ryan Tan <ryantan.mail@gmail.com>
2024-11-15 11:00:50 -0800

    model for addressing rate limit / ai insight page

--- gun_n_powder_backend/restapi/models.py
+++ gun_n_powder_backend/restapi/models.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class ScanLog(models.Model):
-    requester = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to user who requested the scan
-    scan_name = models.CharField(max_length=255, unique=True)  # Unique scan name
+    requester = models.ForeignKey(User, on_delete=models.CASCADE)
+    scan_name = models.CharField(max_length=255, unique=True)
     url = models.URLField()  # URL that was scanned
-    timestamp = models.DateTimeField(auto_now_add=True)  # Time of the request
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class AIInsight(models.Model):
+    scan_name = models.CharField(max_length=255, unique=True)
+    AI_response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
--- gun_n_powder_backend/restapi/views/ai_insight_view.py
+++ gun_n_powder_backend/restapi/views/ai_insight_view.py
@@ -2,27 +2,41 @@
 import os
 import google.generativeai as genai
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.http.response import JsonResponse, HttpResponse
 from rest_framework.views import csrf_exempt
+from ..models import AIInsight
 
 
 @csrf_exempt
 @login_required
 def ai_insight(request):
-    prompt_preheader = """Read this log and tell me next step of actions,
-                        PASS means the test passed and is not a vulnerability. PASS means that the test case is not a vulnerability, stop marking it as a vulnverability.
-                        Give me a summary of the logs and next course of action. """
-    genai.configure(api_key=os.getenv("GOOGLE_LLM_API"))
-    model = genai.GenerativeModel("gemini-1.5-flash")
     if request.method == "GET":
         scanname = request.GET.get("scanname")+".txt"
+        stored_response = AIInsight.objects.filter(scan_name=scanname).first()
+        # If the response is stored, no need to query Google again.
+        if (stored_response):
+            return JsonResponse({"content": stored_response.AI_response})
+        prompt_preheader = """Read this log and tell me next step of actions,
+                        PASS means the test passed and is not a vulnerability. PASS means that the test case is not a vulnerability, stop marking it as a vulnverability.
+                        Give me a summary of the logs and next course of action. """
+        genai.configure(api_key=os.getenv("GOOGLE_LLM_API"))
+        model = genai.GenerativeModel("gemini-1.5-flash")
         file_path = os.path.join("/container_zap", scanname)
         try:
             with open(file_path, "r") as file:
                 content = file.read()
-                response = model.generate_content(prompt_preheader + content)
-                return JsonResponse({"content": response.text})
         except FileNotFoundError:
             return HttpResponse("File not found".encode('utf-8'), status = 404)
         except Exception as e:
             return HttpResponse(f"Error: {e}".encode('utf-8'), status = 500)
+        try:
+            response = model.generate_content(prompt_preheader + content)
+            ai_insight = AIInsight.objects.create(
+                  scan_name=scanname,
+                  AI_response=response.text,
+                  timestamp=timezone.now()
+            )
+            return JsonResponse({"content": response.text})
+        except Exception as e:
+            return HttpResponse(f"Error: {e}".encode('utf-8'), status=500)
