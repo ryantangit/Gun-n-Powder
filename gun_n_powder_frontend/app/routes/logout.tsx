f8742f72a47ae549c6f31fe72fb2c76d54c08571
Denise Gip <denisegip0818@gmail.com>
2024-11-15 21:46:52 -0800

    Added styling for logout page

--- gun_n_powder_frontend/app/routes/logout.tsx
+++ gun_n_powder_frontend/app/routes/logout.tsx
@@ -31,12 +31,14 @@ export async function action({ request }: ActionFunctionArgs) {
 
 export default function LogoutRoute() {
   return (
-    <>
-      <p>Are you sure you want to log out?</p>
+    <div className="flex h-screen items-center justify-center bg-gray-300">
+	  <div className="w-full max-w-xs flex flex-col items-center space-y-8">
+      <p className="text-xl text-gray-700">Are you sure you want to log out?</p>
       <Form method="post">
-        <button>Logout</button>
+        <button type="submit" className="px-6 py-2 text-lg font-light text-gray-700 border border-gray-700 rounded-md hover:bg-gray-200 transition duration-300">Logout</button>
       </Form>
-      <Link to="/dashboard">Never mind</Link>
-    </>
+      <Link to="/dashboard" className="text-base text-gray-700 hover:text-blue-500">Go back to dashboard</Link>
+	  </div>
+    </div>
   );
 }
